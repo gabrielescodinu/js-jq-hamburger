7cd0226ac209261a2cc96ffaee1d1ea6f21b6a01
gabriele <gabrielescodinu96@gmail.com>
2020-11-26 15:25:43 +0100

    commit 01

---
+++ main.js
@@ -0,0 +1,9 @@
+// $('#bottone').click(function(){
+//   $('hamburger-menu a').show();
+// })
+
+var hamburger = $("header-right i");
+
+hamburger.click(function(){
+  $("hamburger-menu").show();
+})
